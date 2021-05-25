32da093a17d77abc5c5b7f7f0a9d4a619166c6e4
cuijansheng <69979960+cuijansheng@users.noreply.github.com>
2021-05-25 13:52:45 +0800

    Update 14.prime.c

--- 14.prime.c
+++ 14.prime.c
@@ -1,13 +1,20 @@
+/*************************************************************************
+	> File Name: 14.prime.c
+	> Author:
+	> Mail:
+	> Created Time: Thu 20 May 2021 05:18:52 PM CST
+ ************************************************************************/
+
 #include <stdio.h>
 #define MAX_N 100
 
 int prime[MAX_N + 5] = {0};
 void init() {
     for (int i = 2; i <= MAX_N; i++) {
-        if (prime[i] == 0) {
-            for (int j = 2 * i; j <= MAX_N; j += i) {
-                prime[j] = 1;
-            }
+        if (prime[i]) continue;
+        prime[++prime[0]] = i;
+        for (int j = i; j <= MAX_N / i; j ++) {
+            prime[j * i] = 1;
         }
     }
     return ;
@@ -15,10 +22,9 @@ void init() {
 
 int main() {
     init();
-    for (int i = 2; i <= MAX_N; i++) {
-        if (prime[i] == 0) {
-            printf("%d\n", i);
-        }
-    }
+   /* for (int i = 1; i <= prime[0]; i++) {
+    printf("%d\n", prime[i]);
+    }*/
+    printf("%d\n", prime[0]);
     return 0;
 }
